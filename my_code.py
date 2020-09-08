fd32ac7b712b9314b99c4bf916e966df1accee02
23aartale <23aartale@athenian.org>
2020-09-08 21:56:25 +0000

    Done

--- my_code.py
+++ my_code.py
@@ -2,11 +2,12 @@
 #  
 
 def times_3(number):
+    number =int(number)
     product = number * 3
     return product
-
 
 if __name__ == '__main__':
 
     x = input("Enter a number: ")
+    product = x
     print(times_3(x))
